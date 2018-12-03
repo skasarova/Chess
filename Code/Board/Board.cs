2424443bc891fdbb4e6c6164fceac307e71354a0
labi <a.yankov.gs@gmail.com>
2018-12-03 12:37:27 +0200

    fixing the earlier commit mistake

--- Code/Board/Board.cs
+++ Code/Board/Board.cs
@@ -7,14 +7,16 @@ namespace Chess.Code.Board
 {
     public class Board
     {
-        public Square[,] board = new Square[8,8];
+	public int PawnInitialCount = 8;
+	public int KingInitialCount = 2;
+	public Square[,] board = new Square[8,8];
         public List<Piece> whitePieces = new List<Piece>();
         public List<Piece> blackPieces = new List<Piece>();
 
         public void FillCollection(List<Piece> pieces)
         {
            //todo: think of a smart way to fill the collection - should every piece class have a collection of the current pieces?
-        }
-        //test
+	   // a loop will add 8 pawns to each side and pieces etc, when generating a piece in startup a method will add it to its respective collection
+	}
     }
 }
